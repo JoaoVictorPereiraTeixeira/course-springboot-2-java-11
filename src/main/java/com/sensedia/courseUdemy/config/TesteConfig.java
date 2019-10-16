7da9946bf2137452a59eeda9cea7a477d4ea6b43
João Victor Teixeira <joaovictorteixeira9819@gmail.com>
2019-10-16 11:39:25 -0300

    Payment, one-to-one association

--- src/main/java/com/sensedia/courseUdemy/config/TesteConfig.java
+++ src/main/java/com/sensedia/courseUdemy/config/TesteConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.sensedia.courseUdemy.entities.Category;
 import com.sensedia.courseUdemy.entities.Order;
 import com.sensedia.courseUdemy.entities.OrderItem;
+import com.sensedia.courseUdemy.entities.Payment;
 import com.sensedia.courseUdemy.entities.Product;
 import com.sensedia.courseUdemy.entities.User;
 import com.sensedia.courseUdemy.entities.enums.OrderStatus;
@@ -86,6 +87,11 @@ public class TesteConfig implements CommandLineRunner{
 		
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"),o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 		
 	}
